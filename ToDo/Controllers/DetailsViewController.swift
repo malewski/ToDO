5a0bb45f83f193e9ec53129b41c8e45f1fe88802
Jan Malewski <malewski.js@gmail.com>
2019-07-14 19:09:49 +0200

    Enter the task details screen feature added

---
+++ ToDo/Controllers/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  ToDo
+//
+//  Created by Jan Malewski on 14/07/2019.
+//  Copyright © 2019 Jan Malewski. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    @IBOutlet weak var details: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ ToDo/Controllers/ToDoListViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ToDoListViewController.swift
+//  ToDo
+//
+//  Created by Jan Malewski on 14/07/2019.
+//  Copyright © 2019 Jan Malewski. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    var tasks = [Task]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row].title
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetails", sender: self)
+        
+    }
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add task", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add task", style: .default) { (action) in
+            
+            let newTask = Task()
+            newTask.title = textField.text!
+            self.tasks.append(newTask)
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new task"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
