6ab3eddadaa6967c3979d7eb2e138815d96e46a8
Jan Malewski <malewski.js@gmail.com>
2019-07-16 00:58:59 +0200

    delete task by swipe to left edge added

--- ToDo/Controllers/ToDoListViewController.swift
+++ ToDo/Controllers/ToDoListViewController.swift
@@ -18,6 +18,9 @@ class ToDoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.rowHeight = 80.0
+        
         loadTasks()
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -34,7 +37,8 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! SwipeTableViewCell
+        cell.delegate = self
         
         if let task = tasks?[indexPath.row] {
             
@@ -122,4 +126,35 @@ class ToDoListViewController: UITableViewController {
         
         tableView.reloadData()
     }
+}
+
+extension ToDoListViewController: SwipeTableViewCellDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            if let taskForDeletion = self.tasks?[indexPath.row] {
+                do {
+                    try self.realm.write {
+                        self.realm.delete(taskForDeletion)
+                    }
+                } catch {
+                    print("Error deleting context \(error)")
+                }
+            }
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
 }
